46f27e7f7a1388802b1f91f916742dcff28b3310
Sozai83 <chikushi3.227@gmail.com>
2023-10-24 22:30:21 +1100

    Added 404 status for not_found

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask
+from flask import Flask, jsonify
+
+# Start virtual env - planetary-api\scripts\activate
+# flask --app app.py --debug run
 
 app = Flask(__name__)
 
@@ -9,7 +12,13 @@ def hello_world():
 
 @app.route('/super_simple')
 def super_simple():
-    return 'Hello from the Planetary API'
+    return jsonify(message='Hello from the Planetary API. Boo yah woo yah')
+
+
+@app.route('/not_found')
+def not_found():
+    return jsonify(message='That resource was not found'), 404
+# Explicit code - 404 (200 is default)
 
 
 if __name__ == '__main__':
